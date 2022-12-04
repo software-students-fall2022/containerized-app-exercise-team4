147b35ba4e32d6fd4f18816bb02c68eb3db6b513
Dixit Timilsina <dt1930@nyu.edu>
2022-12-04 00:32:36 -0500

    Added dashboard object statistics

--- web-app-dashboard/app.py
+++ web-app-dashboard/app.py
@@ -82,21 +82,19 @@ def comparison():
     object_averages = [1,2,3,4,5,6,7,8,9,10]
     for i in range(len(object_scores)):
         object_averages[i] = object_scores[i] / total
-        
-    tick_label = ["bb", "bb", "bb", "bb", "bb", "bb", "bb", "bb", "bb", "bb"]
     
-    # x_pos = [0,1,4,7,10,13,16,19,22,25]
     plt.barh(objects,object_averages, color={'red','green','blue','orange','grey','brown','purple','yellow','indigo','violet'})
     plt.tight_layout(pad=3)
     plt.xlabel('Average scores')
     plt.ylabel('Objects')
     plt.title('Object Score Comparisons')
     plt.savefig('./static/images/plot.png')
+    plt.clf()
     return render_template('comparison.html',url='./static/images/plot.png')
 
 @app.route('/statistics')
 def statistics():
-    docs = db.userData.find({}, {"baseball_bat": 1,"eyeglasses": 1,"grapes": 1,"anvil": 1,"laptop": 1,"dumbbell": 1,"sun": 1,"book": 1,"drums": 1,"ladder": 1}).sort('numLogins',-1)
+    docs = db.userData.find({}, {"baseball_bat": 1,"eyeglasses": 1,"grapes": 1,"anvil": 1,"laptop": 1,"dumbbell": 1,"sun": 1,"book": 1,"drums": 1,"ladder": 1})
     scoresForBaseballBat=[]
     scoresForEyeglasses=[]
     scoresForGrapes=[]
@@ -107,6 +105,17 @@ def statistics():
     scoresForBook=[]
     scoresForDrums=[]
     scoresForLadder=[]
+    allScores=[]
+    allScores.append(scoresForBaseballBat)
+    allScores.append(scoresForEyeglasses)
+    allScores.append(scoresForGrapes)
+    allScores.append(scoresForAnvil)
+    allScores.append(scoresForLaptop)
+    allScores.append(scoresForDumbbell)
+    allScores.append(scoresForSun)
+    allScores.append(scoresForBook)
+    allScores.append(scoresForDrums)
+    allScores.append(scoresForLadder)
     objects = ["baseball_bat", "eyeglasses", "grapes", "anvil", "laptop", "dumbbell", "sun", "book", "drums", "ladder"]
     for doc in docs:
         scoresForBaseballBat.append(doc[objects[0]])
@@ -119,8 +128,261 @@ def statistics():
         scoresForBook.append(doc[objects[7]])
         scoresForDrums.append(doc[objects[8]])
         scoresForLadder.append(doc[objects[9]])
-    
+    listOfObjects=[]
+    for i in range(10):
+        failed,average,good,veryGood,excellent,perfect=countsScores(allScores[i])
+        scores={}
+        scores["object_name"]=objects[i]
+        scores["failed"]=failed
+        scores["average"]=average
+        scores["good"]=good
+        scores["veryGood"]=veryGood
+        scores["excellent"]=excellent
+        scores["perfect"]=perfect
+        listOfObjects.append(scores)
+    print(listOfObjects)
     return render_template('statistics.html')
+
+@app.route('/baseball')
+def baseball():
+    docs = db.userData.find({}, {"baseball_bat": 1})
+    scoresForBaseballBat=[]
+    for doc in docs:
+        scoresForBaseballBat.append(doc["baseball_bat"])
+        
+    failed,average,good,veryGood,excellent,perfect=countsScores(scoresForBaseballBat)
+    y=[]
+    y.append(failed)
+    y.append(average)
+    y.append(good)
+    y.append(veryGood)
+    y.append(excellent)
+    y.append(perfect)
+    x=["Failed","Average","Good","Very Good","Excellent","Perfect"]
+    plt.barh(x,y, color={'red','green','blue','orange','grey','brown'})
+    plt.tight_layout(pad=3)
+    plt.xlabel('Counts')
+    plt.ylabel('Labels')
+    plt.title('Baseball Statistics')
+    plt.savefig('./static/images/baseball.png')
+    plt.clf()
+    return render_template('display.html',url='./static/images/baseball.png')
+
+@app.route('/eyeglasses')
+def eyeglasses():
+    docs = db.userData.find({}, {"eyeglasses": 1})
+    scoresForEyeglasses=[]
+    for doc in docs:
+        scoresForEyeglasses.append(doc["eyeglasses"])
+    failed,average,good,veryGood,excellent,perfect=countsScores(scoresForEyeglasses)
+    y=[]
+    y.append(failed)
+    y.append(average)
+    y.append(good)
+    y.append(veryGood)
+    y.append(excellent)
+    y.append(perfect)
+    x=["Failed","Average","Good","Very Good","Excellent","Perfect"]
+    plt.barh(x,y, color={'red','green','blue','orange','grey','brown'})
+    plt.tight_layout(pad=3)
+    plt.xlabel('Counts')
+    plt.ylabel('Labels')
+    plt.title('Eyeglasses Statistics')
+    plt.savefig('./static/images/eyeglasses.png')
+    plt.clf()
+    return render_template('display.html',url='./static/images/eyeglasses.png')
+
+@app.route('/grapes')
+def grapes():
+    docs = db.userData.find({}, {"grapes": 1})
+    scoresForGrapes=[]
+    for doc in docs:
+        scoresForGrapes.append(doc["grapes"])
+    failed,average,good,veryGood,excellent,perfect=countsScores(scoresForGrapes)
+    y=[]
+    y.append(failed)
+    y.append(average)
+    y.append(good)
+    y.append(veryGood)
+    y.append(excellent)
+    y.append(perfect)
+    x=["Failed","Average","Good","Very Good","Excellent","Perfect"]
+    plt.barh(x,y, color={'red','green','blue','orange','grey','brown'})
+    plt.tight_layout(pad=3)
+    plt.xlabel('Counts')
+    plt.ylabel('Labels')
+    plt.title('Grapes Statistics')
+    plt.savefig('./static/images/grapes.png')
+    plt.clf()
+    return render_template('display.html',url='./static/images/grapes.png')
+
+@app.route('/anvil')
+def anvil():
+    docs = db.userData.find({}, {"anvil": 1})
+    scoresForAnvil=[]
+    for doc in docs:
+        scoresForAnvil.append(doc["anvil"])
+    failed,average,good,veryGood,excellent,perfect=countsScores(scoresForAnvil)
+    y=[]
+    y.append(failed)
+    y.append(average)
+    y.append(good)
+    y.append(veryGood)
+    y.append(excellent)
+    y.append(perfect)
+    x=["Failed","Average","Good","Very Good","Excellent","Perfect"]
+    plt.barh(x,y, color={'red','green','blue','orange','grey','brown'})
+    plt.tight_layout(pad=3)
+    plt.xlabel('Counts')
+    plt.ylabel('Labels')
+    plt.title('Anvil Statistics')
+    plt.savefig('./static/images/anvil.png')
+    plt.clf()
+    return render_template('display.html',url='./static/images/anvil.png')
+
+@app.route('/laptop')
+def laptop():
+    docs = db.userData.find({}, {"laptop": 1})
+    scoresForLaptop=[]
+    for doc in docs:
+        scoresForLaptop.append(doc["laptop"])
+    failed,average,good,veryGood,excellent,perfect=countsScores(scoresForLaptop)
+    y=[]
+    y.append(failed)
+    y.append(average)
+    y.append(good)
+    y.append(veryGood)
+    y.append(excellent)
+    y.append(perfect)
+    x=["Failed","Average","Good","Very Good","Excellent","Perfect"]
+    plt.barh(x,y, color={'red','green','blue','orange','grey','brown'})
+    plt.tight_layout(pad=3)
+    plt.xlabel('Counts')
+    plt.ylabel('Labels')
+    plt.title('Laptop Statistics')
+    plt.savefig('./static/images/laptop.png')
+    plt.clf()
+    return render_template('display.html',url='./static/images/laptop.png')
+
+@app.route('/dumbbell')
+def dumbbell():
+    docs = db.userData.find({}, {"dumbbell": 1})
+    scoresForDumbbell=[]
+    for doc in docs:
+        scoresForDumbbell.append(doc["dumbbell"])
+    failed,average,good,veryGood,excellent,perfect=countsScores(scoresForDumbbell)
+    y=[]
+    y.append(failed)
+    y.append(average)
+    y.append(good)
+    y.append(veryGood)
+    y.append(excellent)
+    y.append(perfect)
+    x=["Failed","Average","Good","Very Good","Excellent","Perfect"]
+    plt.barh(x,y, color={'red','green','blue','orange','grey','brown'})
+    plt.tight_layout(pad=3)
+    plt.xlabel('Counts')
+    plt.ylabel('Labels')
+    plt.title('Dumbbell Statistics')
+    plt.savefig('./static/images/dumbbell.png')
+    plt.clf()
+    return render_template('display.html',url='./static/images/dumbbell.png')
+
+@app.route('/sun')
+def sun():
+    docs = db.userData.find({}, {"sun": 1})
+    scoresForSun=[]
+    for doc in docs:
+        scoresForSun.append(doc["sun"])
+    failed,average,good,veryGood,excellent,perfect=countsScores(scoresForSun)
+    y=[]
+    y.append(failed)
+    y.append(average)
+    y.append(good)
+    y.append(veryGood)
+    y.append(excellent)
+    y.append(perfect)
+    x=["Failed","Average","Good","Very Good","Excellent","Perfect"]
+    plt.barh(x,y, color={'red','green','blue','orange','grey','brown'})
+    plt.tight_layout(pad=3)
+    plt.xlabel('Counts')
+    plt.ylabel('Labels')
+    plt.title('Sun Statistics')
+    plt.savefig('./static/images/sun.png')
+    plt.clf()
+    return render_template('display.html',url='./static/images/sun.png')
+
+@app.route('/book')
+def book():
+    docs = db.userData.find({}, {"book": 1})
+    scoresForBook=[]
+    for doc in docs:
+        scoresForBook.append(doc["book"])
+    failed,average,good,veryGood,excellent,perfect=countsScores(scoresForBook)
+    y=[]
+    y.append(failed)
+    y.append(average)
+    y.append(good)
+    y.append(veryGood)
+    y.append(excellent)
+    y.append(perfect)
+    x=["Failed","Average","Good","Very Good","Excellent","Perfect"]
+    plt.barh(x,y, color={'red','green','blue','orange','grey','brown'})
+    plt.tight_layout(pad=3)
+    plt.xlabel('Counts')
+    plt.ylabel('Labels')
+    plt.title('Book Statistics')
+    plt.savefig('./static/images/book.png')
+    plt.clf()
+    return render_template('display.html',url='./static/images/book.png')
+
+@app.route('/drums')
+def drums():
+    docs = db.userData.find({}, {"drums": 1})
+    scoresForDrums=[]
+    for doc in docs:
+        scoresForDrums.append(doc["drums"])
+    failed,average,good,veryGood,excellent,perfect=countsScores(scoresForDrums)
+    y=[]
+    y.append(failed)
+    y.append(average)
+    y.append(good)
+    y.append(veryGood)
+    y.append(excellent)
+    y.append(perfect)
+    x=["Failed","Average","Good","Very Good","Excellent","Perfect"]
+    plt.barh(x,y, color={'red','green','blue','orange','grey','brown'})
+    plt.tight_layout(pad=3)
+    plt.xlabel('Counts')
+    plt.ylabel('Labels')
+    plt.title('Drums Statistics')
+    plt.savefig('./static/images/drums.png')
+    plt.clf()
+    return render_template('display.html',url='./static/images/drums.png')
+
+@app.route('/ladder')
+def ladder():
+    docs = db.userData.find({}, {"ladder": 1})
+    scoresForLadder=[]
+    for doc in docs:
+        scoresForLadder.append(doc["ladder"])
+    failed,average,good,veryGood,excellent,perfect=countsScores(scoresForLadder)
+    y=[]
+    y.append(failed)
+    y.append(average)
+    y.append(good)
+    y.append(veryGood)
+    y.append(excellent)
+    y.append(perfect)
+    x=["Failed","Average","Good","Very Good","Excellent","Perfect"]
+    plt.barh(x,y, color={'red','green','blue','orange','grey','brown'})
+    plt.tight_layout(pad=3)
+    plt.xlabel('Counts')
+    plt.ylabel('Labels')
+    plt.title('Ladder Statistics')
+    plt.savefig('./static/images/ladder.png')
+    plt.clf()
+    return render_template('display.html',url='./static/images/ladder.png')
 
 if __name__ == "__main__":
     PORT = os.getenv('PORT', 5000) # use the PORT environment variable, or default to 5000
