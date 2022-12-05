0c3598983c671b5d2dac233445789fd5b438031a
kxg202 <72101229+kxg202@users.noreply.github.com>
2022-12-04 22:33:48 -0500

    Update conftest.py

--- machine-learning-client/tests/conftest.py
+++ machine-learning-client/tests/conftest.py
@@ -5,11 +5,3 @@ from app import app
 def flask_app():
     with app.test_client() as client:
         yield client
-
-@pytest.fixture
-def app():
-    yield app
-
-@pytest.fixture
-def client(app):
-    return app.test_client()
