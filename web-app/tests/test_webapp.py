0592e1cbcab802843244d96b3f3f282c44ec5e49
elyazya <113930251+elyazya@users.noreply.github.com>
2022-12-05 01:09:21 -0500

    tests

--- web-app/tests/test_webapp.py
+++ web-app/tests/test_webapp.py
@@ -58,3 +58,75 @@ def test_anvil_path(flask_app):
 def test_book_status(flask_app):
     res=flask_app.get('/book')
     assert res.status_code == 200
+
+
+def test_book_path(flask_app):
+    url = '/book'
+    res = flask_app.get(url)
+    assert res.request.path == url
+
+
+def test_drums_status(flask_app):
+    res = flask_app.get('/drums')
+    assert res.status_code == 200
+
+
+def test_drums_path(flask_app):
+    url = '/drums'
+    res = flask_app.get(url)
+    assert res.request.path == url
+
+
+def test_dumbbell_status(flask_app):
+    res = flask_app.get('/dumbbell')
+    assert res.status_code == 200
+
+
+def test_dumbbell_path(flask_app):
+    url = '/dumbbell'
+    res = flask_app.get(url)
+    assert res.request.path == url
+
+
+def test_eyeglasses_status(flask_app):
+    res = flask_app.get('/eyeglasses')
+    assert res.status_code == 200
+
+
+def test_eyeglasses_path(flask_app):
+    url = '/eyeglasses'
+    res = flask_app.get(url)
+    assert res.request.path == url
+
+
+def test_grapes_status(flask_app):
+    res = flask_app.get('/grapes')
+    assert res.status_code == 200
+
+
+def test_grapes_path(flask_app):
+    url = '/grapes'
+    res = flask_app.get(url)
+    assert res.request.path == url
+
+
+def test_ladder_status(flask_app):
+    res = flask_app.get('/ladder')
+    assert res.status_code == 200
+
+
+def test_ladder_path(flask_app):
+    url = '/ladder'
+    res = flask_app.get(url)
+    assert res.request.path == url
+
+
+def test_laptop_status(flask_app):
+    res = flask_app.get('/laptop')
+    assert res.status_code == 200
+
+
+def test_laptop_path(flask_app):
+    url = '/laptop'
+    res = flask_app.get(url)
+    assert res.request.path == url
